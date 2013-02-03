e13152f080af087fd2c877615fc45725c73df892
Michael Hale <mike@hales.ws>
2013-02-02 23:55:54 -0500

    fix for ruby-electric and yasnippet

--- init.el
+++ init.el
@@ -45,6 +45,19 @@
 (setq yas/root-directory "~/.emacs.d/vendor/yasnippets")
 (yas/load-directory yas/root-directory)
 
+;; Yasnippet workaround for ruby-electric-mode
+;; See: http://code.google.com/p/yasnippet/issues/detail?id=71
+(defun yas/advise-indent-function (function-symbol)
+  (eval `(defadvice ,function-symbol (around yas/try-expand-first activate)
+           ,(format
+             "Try to expand a snippet before point, then call `%s' as usual"
+             function-symbol)
+           (let ((yas/fallback-behavior nil))
+             (unless (and (interactive-p)
+                          (yas/expand))
+               ad-do-it)))))
+(yas/advise-indent-function 'ruby-indent-line)
+
 ;; Theme
 ;;
 
