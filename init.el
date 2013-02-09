b733e13bb135c3430256a8ce75540f4772d2738d
Michael Hale <mike@hales.ws>
2013-02-08 20:47:18 -0500

    add global shortcut to duplicate a line

--- init.el
+++ init.el
@@ -108,6 +108,28 @@
 
 (defun kill-current-buffer () (interactive) (kill-buffer (buffer-name)))
 
+
+(defun duplicate-line-or-region (&optional n)
+  "Duplicate current line, or region if active. With argument N, make N copies. With negative N, comment out original line and use the absolute value."
+  ; thanks to http://stackoverflow.com/questions/88399/how-do-i-duplicate-a-whole-line-in-emacs
+  (interactive "*p")
+  (let ((use-region (use-region-p)))
+    (save-excursion
+      (let ((text (if use-region ;Get region if active, otherwise line
+                      (buffer-substring (region-beginning) (region-end))
+                    (prog1 (thing-at-point 'line)
+                      (end-of-line)
+                      (if (< 0 (forward-line 1)) ;Go to beginning of next line, or make a new one
+                          (newline))))))
+        (dotimes (i (abs (or n 1))) ;Insert N times, or once if not specified
+          (insert text))))
+    (if use-region nil ;Only if we're working with a line (not a region)
+      (let ((pos (- (point) (line-beginning-position)))) ;Save column
+        (if (> 0 n) ;Comment out original with negative arg
+            (comment-region (line-beginning-position) (line-end-position)))
+        (forward-line 1)
+        (forward-char pos)))))
+
 ;; Spelling
 ;;
 
@@ -117,6 +139,7 @@
 ;; Keybindings
 ;;
 
+(global-set-key (kbd "C-c d") 'duplicate-line-or-region)
 (global-set-key (kbd "M-/") 'dabbrev-expand)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "M-s") 'save-buffer)
@@ -161,4 +184,3 @@
 ;;
 
 (setq save-abbrevs nil)
-
