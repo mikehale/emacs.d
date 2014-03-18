11f4e0eb88ad8049d50f4c30c809d2dd1929018d
Michael Hale <mike@hales.ws>
2014-03-18 13:59:49 -0400

    sync with osx clipboard

--- init.el
+++ init.el
@@ -213,3 +213,10 @@
       ".txt")))))
 
 (global-set-key (kbd "M-n") 'new-notes-buffer)
+
+(setq interprogram-cut-function
+      (lambda (text &optional push)
+        (let* ((process-connection-type nil)
+               (pbproxy (start-process "pbcopy" "pbcopy" "/usr/local/bin/pbcopy")))
+          (process-send-string pbproxy text)
+          (process-send-eof pbproxy))))
