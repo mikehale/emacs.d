826c06e80d992ccd49e324c4343c8c169af751b3
Michael Hale <mike@hales.ws>
2014-03-31 23:40:26 -0400

    disable autocompete for now

--- personal/auto-complete.el
+++ personal/auto-complete.el
@@ -1,9 +1,9 @@
-(prelude-require-package 'auto-complete)
+;; (prelude-require-package 'auto-complete)
 
-(require 'auto-complete-config)
-(ac-config-default)
-(setq ac-ignore-case nil)
-(add-to-list 'ac-modes 'ruby-mode)
-(add-to-list 'ac-modes 'web-mode)
+;; (require 'auto-complete-config)
+;; (ac-config-default)
+;; (setq ac-ignore-case nil)
+;; (add-to-list 'ac-modes 'ruby-mode)
+;; (add-to-list 'ac-modes 'web-mode)
 
-(ac-set-trigger-key "TAB")
+;; (ac-set-trigger-key "TAB")
