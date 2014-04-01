64a56fb870d938a19218942989332440f7e9d2cf
Michael Hale <mike@hales.ws>
2014-03-31 23:44:37 -0400

    disable ruby-deep-indent-paren yah!!!

--- personal/ruby.el
+++ personal/ruby.el
@@ -2,6 +2,8 @@
                             rbenv
                             bundler))
 
-(global-rbenv-mode)
+(global-rbenv-mode -1)
+(rbenv-use-global)
+(setq ruby-deep-indent-paren nil)
 (add-hook 'ruby-mode-hook
           (lambda()(highlight-indentation-current-column-mode +1)))
